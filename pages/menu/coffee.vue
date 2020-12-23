b7d0bc4b91c9d2089805f0d75c18c939849eb560
Dalton Saffe <daltonsaffe@gmail.com>
2020-12-22 21:24:28 -0500

    add tooltip

--- pages/menu/coffee.vue
+++ pages/menu/coffee.vue
@@ -1,12 +1,18 @@
 <template>
-  <section class="container">
-    <h1>coffee</h1>
-    <h3 class="menu-title">bevs</h3>
+  <section class="menu">
+    <NuxtLink to="/menu"><h1>menu</h1></NuxtLink>
+    <h3 class="menu-title">coffee</h3>
     <ul>
-      <li>coffee</li>
-      <li>butter coffee</li>
-      <li>iced tea</li>
-      <li>lemon seltz</li>
+      <li>choose quality, freshly roasted beans</li>
+      <li>boil water</li>
+      <li>grind to the desired consistency</li>
+      <li>add to brewing method</li>
+      <li class="tooltip">
+        bloom coffee <br /><span class="tooltiptext"
+          >dampen with water and let the gases release for 30-40 seconds</span
+        >
+      </li>
+      <li>continue brewing & enjoy</li>
     </ul>
   </section>
 </template>
@@ -16,12 +22,70 @@ export default {}
 </script>
 
 <style scoped>
-.container {
+.menu {
   display: flex;
   max-width: 700px;
   justify-content: center;
   align-items: center;
   margin: 0 auto;
   flex-direction: column;
+}
+
+.menu-title {
+  font-size: 40px;
+  margin: 20px 0;
+}
+
+h1 {
+  font-size: 55px;
+}
+
+ul {
+  text-align: center;
+}
+
+li {
+  margin: 10px 0 10px;
+}
+.tooltip {
+  position: relative;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 100%;
+  background: radial-gradient(#212121, #222222, #1c1c1c, #131313);
+  color: #fff;
+  text-align: center;
+  border-radius: 10px;
+  padding: 5%;
+  border: 1px solid #fff;
+
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+  left: 0;
+  opacity: 0;
+  top: 0;
+  transition: all 400ms ease;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  cursor: help;
+  opacity: 1;
+}
+
+a {
+  color: #fff;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+.food-list li {
+  margin: 0 0 50px 0;
 }
 </style>
