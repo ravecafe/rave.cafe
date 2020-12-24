7ccf7f17c5a5b7230ed6e72c9d05b104e7e2d136
Dalton Saffe <daltonsaffe@gmail.com>
2020-12-24 03:08:21 -0500

    add photos and txt

--- pages/we-miss-you/index.vue
+++ pages/we-miss-you/index.vue
@@ -75,42 +75,140 @@
     </section>
     <section class="month-cell">
       <h2>02 - february</h2>
+      <p>Womack & Womack - Teardrops</p>
+      <img src="~/assets/miss-u/02/000015750037.jpg" />
+      <p>i'm 30 betch</p>
+      <img src="~/assets/miss-u/02/000206700014.jpg" />
+      <p>i'm cozy betch</p>
+      <img src="~/assets/miss-u/02/DSCF0510.5132255.jpg" />
     </section>
     <section class="month-cell">
       <h2>03 - march</h2>
+      <p>Shannon - Give Me Tonight</p>
+      <img src="~/assets/miss-u/03/000326160030.7106187.jpg" />
+      <p>it begins...</p>
+      <img src="~/assets/miss-u/03/000326160013.9f0093b.jpg" />
+      <p>pothos bb</p>
+      <img src="~/assets/miss-u/03/DSCF0659.9557cd3.jpg" />
+      <p>bushwick sunset</p>
     </section>
     <section class="month-cell">
       <h2>04 - april</h2>
+      <p>Rene & Angela - I'll Be Good</p>
+      <img src="~/assets/miss-u/04/Screen_Shot_2020-12-23_at_9.38.20_PM.jpg" />
+      <p>just streaming</p>
+      <img src="~/assets/miss-u/04/DSCF0514.162801c.jpg" />
+      <p>pothos bb</p>
+      <img
+        src="~/assets/miss-u/04/1EE5C6A8-9EC9-4D6C-BA24-25C77DF065E2_1_105_c.jpeg"
+      />
     </section>
     <section class="month-cell">
       <h2>05 - may</h2>
+      <p>Kleer - Tonight</p>
+      <img src="~/assets/miss-u/05/A2665669-AA80-4353-BE14-6B7075489E8A.jpg" />
+      <img src="~/assets/miss-u/05/DSCF0638.b9f01c2.jpg" />
+      <img
+        src="~/assets/miss-u/05/E01FCCF1-201D-4B5F-B352-C16C1F2F8985_1_105_c.jpeg"
+      />
+      <img src="~/assets/miss-u/05/DSCF0638.b9f01c2.jpg" />
+      <img src="~/assets/miss-u/05/DSCF0782.820f2cc.jpg" />
     </section>
     <section class="month-cell">
       <h2>06 - june</h2>
+      <p>Future Sounds of London - Papua New Guinea</p>
+      <img src="~/assets/miss-u/06/DSCF0802.eedcbce.jpg" />
+      <p>Black & Latinx Trans Lives Matter</p>
+      <img src="~/assets/miss-u/06/DSCF0755.jpg" />
+      <img
+        src="~/assets/miss-u/06/283A3B6E-D1F6-4ED1-99FB-769D63B4ADEE_1_105_c.jpeg"
+      />
+      <p>rainbows above the protest</p>
+      <img src="~/assets/miss-u/06/000125600032.327eae3.jpg" />
+      <p>we love u nowadays</p>
     </section>
     <section class="month-cell">
       <h2>07 - july</h2>
+      <p>Great Ghost - Strange Encounter With Light (feat. Liai)</p>
+      <img src="~/assets/miss-u/07/000125560023.ef92a11.jpg" />
+      <img src="~/assets/miss-u/07/DSCF0890.c942930.jpg" />
+      <p>moments in nature</p>
+      <img src="~/assets/miss-u/07/AC127437-0260-4C1F-82E8-E9AB8BB79609.jpg" />
+      <img
+        src="~/assets/miss-u/07/9A45E483-1FAE-436D-91B7-013CEA691460_1_105_c.jpeg"
+      />
+      <img src="~/assets/miss-u/07/000326160031.cf79374.jpg" />
     </section>
     <section class="month-cell">
       <h2>08 - august</h2>
-      <video muted autoplay class="media-center">
+      <p>Stray - When It Rains</p>
+      <img src="~/assets/miss-u/08/000125570026.3252acb.jpg" />
+      <img src="~/assets/miss-u/08/IMG_6184.jpeg" />
+      <p>we built a sand system w/ aileen and adi</p>
+      <img src="~/assets/miss-u/08/000125630029.jpg" />
+      <p>park hangs + good food = yes plz</p>
+      <video muted autoplay loop class="media-center">
         <source
           src="https://media.giphy.com/media/XETrg0yyG3jOYrADq9/giphy.mp4"
           type="video/mp4"
         />
       </video>
+      <p>covid compliance or bust</p>
+      <img src="~/assets/miss-u/08/000125550014.b636b6b.jpg" />
+      <p>sculptures w/ jess and pat!!</p>
+      <img src="~/assets/miss-u/08/F7DC387A-79CA-4D01-BB5A-03099A3579AB.jpeg" />
+      <img src="~/assets/miss-u/08/000125570011.jpg" />
+      <p>first time back in the theater</p>
     </section>
     <section class="month-cell">
       <h2>09 - september</h2>
+      <p>Eris Drew - See You In Snow</p>
+      <img src="~/assets/miss-u/09/000125630011.jpg" />
+      <p>magic hour in dumbo</p>
+      <img
+        src="~/assets/miss-u/09/B3EF14C7-8CE4-4C5A-97D0-84F8639A69EE_1_105_c.jpeg"
+      />
+      <img src="~/assets/miss-u/09/000125570014_(1).863e2cd.jpg" />
+      <img src="~/assets/miss-u/09/000125600004.255d358.jpg" />
     </section>
     <section class="month-cell">
       <h2>10 - october</h2>
+      <p>Andy Stott - How It Was</p>
+      <img src="~/assets/miss-u/10/3B76937C-E1BE-41ED-9936-0DF97452AC40.jpg" />
+      <img src="~/assets/miss-u/10/000133070029.025387a.jpg" />
+      <p>big jellyfish NRG</p>
+      <img src="~/assets/miss-u/10/000133070027.d41eec8.jpg" />
     </section>
     <section class="month-cell">
       <h2>11 - november</h2>
+      <p>Psychic Mirrors - Midnight Special</p>
+      <img src="~/assets/miss-u/11/IMG_2713 (1).jpg" />
+      <p>so many autumn park hangs :)</p>
+      <img
+        src="~/assets/miss-u/11/E2561389-4768-436B-BA3F-66A45D8226EA_1_105_c.jpeg"
+      />
+      <p>we brought rüfüs & lydia too!!</p>
+      <img src="~/assets/miss-u/11/000133080002.jpg" />
+      <img src="~/assets/miss-u/11/49480C86-E7FA-46FA-8C7D-AABF180ADC1E.jpeg" />
+      <p>
+        seward park had leaves falling all over it, and it was so cute!! photo
+        by A. Hollowday
+      </p>
+      <img
+        src="~/assets/miss-u/11/D87517B4-AFEC-4E7F-82D7-9CDFACC81FFD_1_105_c.jpeg"
+      />
+      <p>daily writing, yoga, & meditation helped ground us</p>
+      <img src="~/assets/miss-u/11/000326100020.34f0f62.jpg" />
     </section>
     <section class="month-cell">
       <h2>12 - december</h2>
+      <p>Pebbles - Mercedes Boy</p>
+      <img src="~/assets/miss-u/12/7AF06E25-505F-4447-82B6-1F584E31E741.jpg" />
+      <p>oh hayyyy</p>
+      <img src="~/assets/miss-u/12/DSCF1282.fcac765.jpg" />
+      <p>and finally... a couple of moments from last year's photo shoot</p>
+      <img src="~/assets/miss-u/12/DSCF0446.d7e148e.jpg" />
+      <img src="~/assets/miss-u/12/DSCF0471.82135ae.jpg" />
     </section>
   </section>
 </template>
@@ -140,12 +238,14 @@ a:hover {
 .logo-wrap {
   margin: 5% auto 0;
 }
+.media-center {
+  text-align: center;
+}
 .container {
   display: flex;
   max-width: 700px;
   align-items: center;
   justify-content: center;
-
   margin: 0 auto;
   flex-direction: column;
   font-size: 1.25rem;
@@ -153,10 +253,18 @@ a:hover {
 .month-cell {
   margin: 5% 0;
 }
-.month-cell img {
+.month-cell img,
+.month-cell video {
   width: 100%;
   height: auto;
   margin: 5% 0;
+}
+.month-cell p:first-of-type {
+  text-align: right;
+  margin-right: 0;
+  margin: 2% 0;
+  width: 80%;
+  margin-left: auto;
 }
 .month-cell h2,
 h1 {
