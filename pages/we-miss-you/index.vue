56deec377e308be46a9ce9a30cd5023b07284c97
Dalton Saffe <daltonsaffe@gmail.com>
2020-12-26 19:02:32 -0500

    mod miss u pg

--- pages/we-miss-you/index.vue
+++ pages/we-miss-you/index.vue
@@ -5,17 +5,70 @@
       <Logo height="50" />
     </div>
     <p>
-      we miss you, we are thinking of you, and we are very grateful to have you
-      in our lives. here are some moments that have been significant to us in
-      2020. we hope they also bring you joy!
+      we miss you, we are thinking of you, and we are grateful to have you in
+      our lives. here are some moments that have been significant to us in 2020.
+      we hope they also bring you joy!
     </p>
-    <p class="center">&lt;3</p>
+
     <p>
       we are exploring new ways of staying connected outside of social media.
       drop your email below if you'd like to learn more about
       <a href="https://rave.cafe">rave.cafe</a> &amp; receive january's menu
     </p>
+    <p class="center">&lt;3</p>
     <p class="center">L&amp;D</p>
+    <div id="mc_embed_signup">
+      <form
+        action="https://cafe.us7.list-manage.com/subscribe/post?u=7ac7e285f8d8826591ef315a7&amp;id=47d255f050"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        class="validate"
+        target="_blank"
+        novalidate
+      >
+        <div id="mc_embed_signup_scroll">
+          <div class="mc-field-group">
+            <input
+              type="email"
+              value=""
+              name="EMAIL"
+              class="email"
+              id="mce-EMAIL"
+            />
+          </div>
+          <div id="mce-responses" class="clear">
+            <div
+              class="response"
+              id="mce-error-response"
+              style="display: none"
+            ></div>
+            <div
+              class="response"
+              id="mce-success-response"
+              style="display: none"
+            ></div>
+          </div>
+          <div style="position: absolute; left: -5000px" aria-hidden="true">
+            <input
+              type="text"
+              name="b_7ac7e285f8d8826591ef315a7_47d255f050"
+              tabindex="-1"
+              value=""
+            />
+          </div>
+          <div class="clear">
+            <input
+              type="submit"
+              value="⮕"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              class="button"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
     <iframe
       width="100%"
       height="166"
@@ -24,36 +77,6 @@
       allow="autoplay"
       src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/953332642&color=%232c2c2c&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false"
     ></iframe>
-    <div
-      style="
-        font-size: 10px;
-        color: #cccccc;
-        line-break: anywhere;
-        word-break: normal;
-        overflow: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        font-family: Interstate, Lucida Grande, Lucida Sans Unicode, Lucida Sans,
-          Garuda, Verdana, Tahoma, sans-serif;
-        font-weight: 100;
-      "
-    >
-      <a
-        href="https://soundcloud.com/ravecafe"
-        title="rave.cafe"
-        target="_blank"
-        style="color: #cccccc; text-decoration: none"
-        >rave.cafe</a
-      >
-      ·
-      <a
-        href="https://soundcloud.com/ravecafe/ld-2020cafe"
-        title="L&amp;D - 2020"
-        target="_blank"
-        style="color: #cccccc; text-decoration: none"
-        >L&amp;D - 2020</a
-      >
-    </div>
     <section class="month-cell">
       <h2>01 - january</h2>
       <p>Val Young - Seduction</p>
@@ -62,43 +85,30 @@
         class="vertical"
         src="~/assets/miss-u/01/A7238C14-B2D0-424C-A47E-E26427D3EB62.jpeg"
       />
-      <p>we love you mood ring</p>
       <img
         src="~/assets/miss-u/01/10210662-9598-4D15-8668-E660C43F9555_1_105_c.jpeg"
       />
-      <p>
-        "the world's loudest photo" 35mm, taken during a serene moment at
-        ambient church
-      </p>
       <img src="~/assets/miss-u/01/000065770010.869265a.jpg" />
-      <p>foreshadowing</p>
     </section>
     <section class="month-cell">
       <h2>02 - february</h2>
       <p>Womack & Womack - Teardrops</p>
       <img src="~/assets/miss-u/02/000015750037.jpg" />
-      <p>i'm 30 betch</p>
       <img src="~/assets/miss-u/02/000206700014.jpg" />
-      <p>i'm cozy betch</p>
       <img src="~/assets/miss-u/02/DSCF0510.5132255.jpg" />
     </section>
     <section class="month-cell">
       <h2>03 - march</h2>
       <p>Shannon - Give Me Tonight</p>
       <img src="~/assets/miss-u/03/000326160030.7106187.jpg" />
-      <p>it begins...</p>
       <img src="~/assets/miss-u/03/000326160013.9f0093b.jpg" />
-      <p>pothos bb</p>
       <img src="~/assets/miss-u/03/DSCF0659.9557cd3.jpg" />
-      <p>bushwick sunset</p>
     </section>
     <section class="month-cell">
       <h2>04 - april</h2>
       <p>Rene & Angela - I'll Be Good</p>
       <img src="~/assets/miss-u/04/Screen_Shot_2020-12-23_at_9.38.20_PM.jpg" />
-      <p>just streaming</p>
       <img src="~/assets/miss-u/04/DSCF0514.162801c.jpg" />
-      <p>pothos bb</p>
       <img
         src="~/assets/miss-u/04/1EE5C6A8-9EC9-4D6C-BA24-25C77DF065E2_1_105_c.jpeg"
       />
@@ -111,28 +121,26 @@
       <img
         src="~/assets/miss-u/05/E01FCCF1-201D-4B5F-B352-C16C1F2F8985_1_105_c.jpeg"
       />
-      <img src="~/assets/miss-u/05/DSCF0638.b9f01c2.jpg" />
+      <img
+        src="~/assets/miss-u/05/303C0A41-235B-494C-9AA1-6D17642DF761_1_105_c.jpeg"
+      />
       <img src="~/assets/miss-u/05/DSCF0782.820f2cc.jpg" />
     </section>
     <section class="month-cell">
       <h2>06 - june</h2>
       <p>Future Sounds of London - Papua New Guinea</p>
       <img src="~/assets/miss-u/06/DSCF0802.eedcbce.jpg" />
-      <p>Black & Latinx Trans Lives Matter</p>
       <img src="~/assets/miss-u/06/DSCF0755.jpg" />
       <img
         src="~/assets/miss-u/06/283A3B6E-D1F6-4ED1-99FB-769D63B4ADEE_1_105_c.jpeg"
       />
-      <p>rainbows above the protest</p>
       <img src="~/assets/miss-u/06/000125600032.327eae3.jpg" />
-      <p>we love u nowadays</p>
     </section>
     <section class="month-cell">
       <h2>07 - july</h2>
       <p>Great Ghost - Strange Encounter With Light (feat. Liai)</p>
       <img src="~/assets/miss-u/07/000125560023.ef92a11.jpg" />
       <img src="~/assets/miss-u/07/DSCF0890.c942930.jpg" />
-      <p>moments in nature</p>
       <img src="~/assets/miss-u/07/AC127437-0260-4C1F-82E8-E9AB8BB79609.jpg" />
       <img
         src="~/assets/miss-u/07/9A45E483-1FAE-436D-91B7-013CEA691460_1_105_c.jpeg"
@@ -144,27 +152,21 @@
       <p>Stray - When It Rains</p>
       <img src="~/assets/miss-u/08/000125570026.3252acb.jpg" />
       <img src="~/assets/miss-u/08/IMG_6184.jpeg" />
-      <p>we built a sand system w/ aileen and adi</p>
       <img src="~/assets/miss-u/08/000125630029.jpg" />
-      <p>park hangs + good food = yes plz</p>
       <video muted autoplay loop class="media-center">
         <source
           src="https://media.giphy.com/media/XETrg0yyG3jOYrADq9/giphy.mp4"
           type="video/mp4"
         />
       </video>
-      <p>covid compliance or bust</p>
       <img src="~/assets/miss-u/08/000125550014.b636b6b.jpg" />
-      <p>sculptures w/ jess and pat!!</p>
       <img src="~/assets/miss-u/08/F7DC387A-79CA-4D01-BB5A-03099A3579AB.jpeg" />
       <img src="~/assets/miss-u/08/000125570011.jpg" />
-      <p>first time back in the theater</p>
     </section>
     <section class="month-cell">
       <h2>09 - september</h2>
       <p>Eris Drew - See You In Snow</p>
       <img src="~/assets/miss-u/09/000125630011.jpg" />
-      <p>magic hour in dumbo</p>
       <img
         src="~/assets/miss-u/09/B3EF14C7-8CE4-4C5A-97D0-84F8639A69EE_1_105_c.jpeg"
       />
@@ -176,37 +178,27 @@
       <p>Andy Stott - How It Was</p>
       <img src="~/assets/miss-u/10/3B76937C-E1BE-41ED-9936-0DF97452AC40.jpg" />
       <img src="~/assets/miss-u/10/000133070029.025387a.jpg" />
-      <p>big jellyfish NRG</p>
       <img src="~/assets/miss-u/10/000133070027.d41eec8.jpg" />
     </section>
     <section class="month-cell">
       <h2>11 - november</h2>
       <p>Psychic Mirrors - Midnight Special</p>
       <img src="~/assets/miss-u/11/IMG_2713 (1).jpg" />
-      <p>so many autumn park hangs :)</p>
       <img
         src="~/assets/miss-u/11/E2561389-4768-436B-BA3F-66A45D8226EA_1_105_c.jpeg"
       />
-      <p>we brought rüfüs & lydia too!!</p>
       <img src="~/assets/miss-u/11/000133080002.jpg" />
       <img src="~/assets/miss-u/11/49480C86-E7FA-46FA-8C7D-AABF180ADC1E.jpeg" />
-      <p>
-        seward park had leaves falling all over it, and it was so cute!! photo
-        by A. Hollowday
-      </p>
       <img
         src="~/assets/miss-u/11/D87517B4-AFEC-4E7F-82D7-9CDFACC81FFD_1_105_c.jpeg"
       />
-      <p>daily writing, yoga, & meditation helped ground us</p>
       <img src="~/assets/miss-u/11/000326100020.34f0f62.jpg" />
     </section>
     <section class="month-cell">
       <h2>12 - december</h2>
       <p>Pebbles - Mercedes Boy</p>
       <img src="~/assets/miss-u/12/7AF06E25-505F-4447-82B6-1F584E31E741.jpg" />
-      <p>oh hayyyy</p>
       <img src="~/assets/miss-u/12/DSCF1282.fcac765.jpg" />
-      <p>and finally... a couple of moments from last year's photo shoot</p>
       <img src="~/assets/miss-u/12/DSCF0446.d7e148e.jpg" />
       <img src="~/assets/miss-u/12/DSCF0471.82135ae.jpg" />
     </section>
@@ -218,6 +210,9 @@ export default {}
 </script>
 
 <style scoped>
+.logo-wrap {
+  margin: 10px auto 0;
+}
 .title-right {
   text-align: right;
   margin: 20px 0 0;
@@ -241,6 +236,10 @@ a:hover {
 .media-center {
   text-align: center;
 }
+iframe {
+  margin: 10px 0 40px;
+  opacity: 0.7;
+}
 .container {
   display: flex;
   max-width: 700px;
@@ -260,11 +259,8 @@ a:hover {
   margin: 5% 0;
 }
 .month-cell p:first-of-type {
-  text-align: right;
   margin-right: 0;
   margin: 2% 0;
-  width: 80%;
-  margin-left: auto;
 }
 .month-cell h2,
 h1 {
@@ -274,6 +270,59 @@ h1 {
 .month-cell p {
   margin: 2% 10% 2% 0;
   font-size: 1.25rem;
+}
+.email {
+  color: #fff;
+  border: 1px solid #fff;
+  background-image: none;
+  background-color: transparent;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+  font-size: 20px;
+  padding: 10px;
+  font-family: inherit;
+  width: 100%;
+}
+.button {
+  display: inline-block;
+  border: 1px solid #fff;
+  padding: 1rem;
+  margin: 20px;
+  width: 100%;
+  font-size: 2rem;
+  text-decoration: none;
+  background: transparent;
+  color: #ffffff;
+  font-family: sans-serif;
+  font-size: 1rem;
+  cursor: pointer;
+  text-align: center;
+  transition: background 250ms ease-in-out, transform 150ms ease;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+}
+
+.button:focus {
+  outline: 1px solid #fff;
+  outline-offset: -4px;
+}
+
+button:active {
+  transform: scale(0.99);
+}
+#mc_embed_signup {
+  margin: 50px 0;
+  width: 100%;
+}
+#mc_embed_signup_scroll {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 @media only screen and (max-width: 850px) {
   .container {
