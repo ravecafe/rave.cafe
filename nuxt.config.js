d3694779b39d30c54ac9e35d55e01616c9dabe5b
Dalton Saffe <daltonsaffe@gmail.com>
2021-04-06 20:13:42 -0400

    add descrip meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'A gentle reminder that there are no rules',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/rc-smile.png' }],
   },
